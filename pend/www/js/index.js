cfab12dd527d475c4b037bcb312bca53bb0c4f68
Kousuke Abe <seikou2202@gmail.com>
2018-06-08 21:28:43 +0900

    Second commit

--- pend/www/js/index.js
+++ pend/www/js/index.js
@@ -1,25 +1,8 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+      document.getElementById("torihiki").addEventListener('click', this.displayHello.bind(this), false);
+      //  document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
     // deviceready Event Handler
@@ -28,6 +11,29 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+    },
+
+    displayHello: function(){
+      //HTTP通信をする所
+      var xhr = new XMLHttpRequest();
+
+      // ハンドラの登録.
+      xhr.onload = function() {
+          switch ( xhr.readyState ) {
+              case 4: // データ受信完了.
+                  if( xhr.status == 200 || xhr.status == 304 ) {
+                    console.log(xhr.responseText);
+                      var data = xhr.responseText; // responseXML もあり
+                      document.getElementById("coin").value = data;
+                  } else {
+                      console.log( 'Failed. HttpStatus: '+xhr.statusText );
+                  }
+                  break;
+          }
+      };
+      xhr.open('GET', 'http://192.168.2.76:2525', false);
+      xhr.send(null);
+      xhr.abort(); // 再利用する際にも abort() しないと再利用できないらしい. ←そういうことらしい
     },
 
     // Update DOM on a Received Event
