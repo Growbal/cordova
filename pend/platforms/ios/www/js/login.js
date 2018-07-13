ad44957b113b58c1d396b5545c5e37663a9e80cc
TakayaOga <akitsubame010@gmail.com>
2018-07-13 21:19:51 +0900

    configと上書きされていたものを直した

---
+++ pend/platforms/ios/www/js/login.js
@@ -0,0 +1,60 @@
+var app = {
+    // Application Constructor。アプリ起動時に実行する関数かな
+    initialize: function() {
+      document.getElementById("submit").addEventListener('click', this.SendMoney.bind(this), false);
+      //  document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+      console.log("ds");
+        this.receivedEvent('deviceready');
+    },
+
+    SendMoney: function(){
+      //HTTP通信をする所
+      var xhr = new XMLHttpRequest();
+      //ここも大事。　DOMどすえ
+      let username = document.getElementById('username').value;
+      let password = document.getElementById('password').value;
+
+      // ハンドラの登録.
+      xhr.onload = function() {
+          //readyState ... 送っている間の状況を見ることができる。すげえやつ
+          switch ( xhr.readyState ) {
+              case 4: // データ受信完了.
+                // 200は無事に送れたという意味 300はぐぐって
+                  if( xhr.status == 200 || xhr.status == 304 ) {
+                      //さっきのwelcomeうほうほは　responseTextの中に入っているッッ!!
+                      app.changePage(JSON.parse(xhr.response));
+                      document.getElementById('username').value = data.length;
+                  } else {
+                    //エラー処理
+                      console.log( 'Failed. HttpStatus: '+ xhr.statusText );
+                  }
+                  break;
+          }
+      };
+
+      //どこのサーバーに送るかを指定する
+      //サーバーへの送り方は２種類ある。 GETとPOSTの２種類がある。
+      //GETはフロントからデータを送らないリクエスト。POSTはフロントからデータを送るリクエスト
+      xhr.open('GET', `http://jupiter.tntetsu-lab.cs.kanagawa-it.ac.jp/Login?username=${username}&password=${password}`, false);
+      // POST 送信の場合は Content-Type は固定.
+      //openだけでは送れていない。sendをすることで送ったことになる
+      xhr.send("");
+      xhr.abort(); // 再利用する際にも abort() しないと再利用できないらしい. ←そういうことらしい
+    },
+
+    changePage: function(data){
+      if(data.length == 0){
+        document.getElementById("error").innerHTML = "ユーザー名、またはパスワードが違っています"
+        return;
+      }
+      location.href = "./home.html";
+    }
+};
+
+app.initialize();
